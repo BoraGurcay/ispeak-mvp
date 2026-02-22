493ef13eccd88b1dd717efb4dd729b9e0e1979de
BoraGurcay <yacrugarob@gmail.com>
2026-02-21 20:11:34 -0500

    add service worker content

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+/* Minimal Service Worker for PWA install */
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Required for Chrome installability
+self.addEventListener("fetch", (event) => {
+  // network passthrough
+});
